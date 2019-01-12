f76e3b8d163cd3b1662eb95d41b9eb1849115f08
leiqk <leiqk@dxy.cn>
2019-01-12 11:46:13 +0800

    [add]新增测试模块

---
+++ test/__init__.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python  
+# encoding: utf-8  
+
+""" 
+@author: lqk  
+@contact: 798244092@qq.com 
+@site: https://github.com/lqkweb 
+@file: __init__.py.py 
+@time: 2019/1/12 上午11:45 
+"""
+
+if __name__ == '__main__':
+    '''
+    mysql验证 ：
+    1、权限验证   验证通过，直接执行脚本；验证不通过，退出；
+    2、进程验证
+    :param id:
+    :return:
+    '''
+
+    pass
